41a78fafecd936d08bc906b0d658bea03087c637
engrjeff <engineerjeff15@gmail.com>
2026-01-31 21:33:14 +0800

    refactor: add more FAQs

--- components/faq.tsx
+++ components/faq.tsx
@@ -7,6 +7,10 @@ import {
 
 const faq = [
   {
+    question: 'Is this a Christian wedding?',
+    answer: 'A resounding YES!',
+  },
+  {
     question: 'On kids at the wedding',
     answer:
       'In order to allow guests, including parents, to fully relax and enjoy the celebration, we have chosen to make our wedding an adults-only event. We appreciate your understanding and look forward to celebrating with you!',
@@ -15,6 +19,11 @@ const faq = [
     question: 'On being unplugged',
     answer:
       'We invite you to be fully present with us during our ceremony. Kindly refrain from using phones or cameras so that everyone can enjoy the moment together. We will have professional photographers capturing the day for you to cherish later!',
+  },
+  {
+    question: 'On bringing a plus-one',
+    answer:
+      'Though we desire that many people would witness our wedding, there is a limit to the number of guests that we can accomodate so we can only invite selected people.',
   },
   {
     question: 'What happens if I do not RSVP in time?',
@@ -30,6 +39,12 @@ const faq = [
     question: 'Is the venue weather-proof?',
     answer:
       'Yes, the Garden Hive venue for ceremony and reception areas are covered so you can rest assured that weather will not impact the event.',
+  },
+  {
+    question:
+      'Is the reception venue a separate place from where the ceremony will be held?',
+    answer:
+      'The ceremony will be held at the Garden Hive garden while the reception area will be at the Garden Hive Hall. The two areas are just separated by a wall.',
   },
   {
     question: 'Are there parking spaces available at the venue?',
